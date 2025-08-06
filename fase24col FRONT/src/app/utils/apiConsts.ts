30bf4f74a049f8673f8082aeea11fd9730e7d25b
Johan Sebastian Marin Rojas <josmarinr@gmail.com>
2025-08-06 17:53:52 -0500

    Endpoints actualizados

--- fase24col FRONT/src/app/utils/apiConsts.ts
+++ fase24col FRONT/src/app/utils/apiConsts.ts
@@ -7,4 +7,8 @@ export const API_URL = {
   USER_PROFILE: (id: any) => `${BASE_URL}/usuarios/${id}`,
   UPDATE_USER: (id: any) => `${BASE_URL}/usuarios/${id}`,
   DELETE_USER: (id: any) => `${BASE_URL}/usuarios/${id}`,
+  PRODUCTS: `${BASE_URL}/productos`,
+  PRODUCT_DETAILS: (id: any) => `${BASE_URL}/productos/${id}`,
+  CREATE_PRODUCT: `${BASE_URL}/productos`,
+  UPDATE_PRODUCT: (id: any) => `${BASE_URL}/productos/${id}`,
 };
